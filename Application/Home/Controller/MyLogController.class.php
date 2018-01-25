607c2415c1b55fda1aca888503e43597d9f9fe19
lfq <lfq3300@qq.com>
2018-01-25 20:04:54 +0800

    no message

--- Application/Home/Controller/MyLogController.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class MyLogController extends Controller {
-    public function index(){
-        $this->assign("title","个人日志");
-        $this->display();
-    }
-}
--- Application/Runtime/Cache/Admin/6323b6ada38228c21a2baf3086dfcdbd.php
+++
@@ -1,1 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?>add?id=4
